ab314a457c831effac528da5efd5bbe0935cd306
ZavierGhani <zavierghani02@gmail.com>
2024-10-08 22:57:37 +0800

    Create PRAK002-2410817210014-ZAVIER PUTRA NATA GHANI.py

---
+++ Modul-0/Soal 2/PRAK002-2410817210014-ZAVIER PUTRA NATA GHANI.py
@@ -0,0 +1,5 @@
+def cetak_pesan():
+    print("Selamat Pagi, Saya Zavier Putra Nata Ghani")
+    print("Selamat Siang, Saya Zavier Putra Nata Ghani")
+    print("Selamat Malam, Saya Zavier Putra Nata Ghani")
+cetak_pesan()
