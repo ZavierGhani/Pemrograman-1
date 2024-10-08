c96fcb5fa60cc01b583ac4040461fb747fd4d52e
ZavierGhani <zavierghani02@gmail.com>
2024-10-08 22:58:11 +0800

    Create PRAK002-2410817210014-ZAVIERPUTRANATAGHANI.c

---
+++ Modul-0/Soal 2/PRAK002-2410817210014-ZAVIERPUTRANATAGHANI.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (){
+    printf("Selamat Pagi, Zavier Putra Nata Ghani\n");
+    printf("Selamat Siang, Zavier Putra Nata Ghani\n");
+    printf("Selamat Malam, Zavier Putra Nata Ghani\n");
+    return 0;
+}
