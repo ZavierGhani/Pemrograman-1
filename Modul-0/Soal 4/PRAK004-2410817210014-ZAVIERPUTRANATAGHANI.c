be9414ee62217c63081ddbfe96808e4344571fec
ZavierGhani <zavierghani02@gmail.com>
2024-10-08 23:00:43 +0800

    Create PRAK004-2410817210014-ZAVIERPUTRANATAGHANI.c

---
+++ Modul-0/Soal 4/PRAK004-2410817210014-ZAVIERPUTRANATAGHANI.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    printf("###########################\n");
+    printf("#                         #\n");
+    printf("# Zavier Putra Nata Ghani #\n");
+    printf("#      2410817210014      #\n");
+    printf("#                         #\n");
+    printf("###########################\n");
+    return 0;
+}
