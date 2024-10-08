bf66f35b8ada99353f6ab41804945945301a1092
ZavierGhani <zavierghani02@gmail.com>
2024-10-08 23:01:51 +0800

    Create PRAK005-2410817210014-ZAVIER PUTRA NATA GHANI.py

---
+++ Modul-0/Soal 5/PRAK005-2410817210014-ZAVIER PUTRA NATA GHANI.py
@@ -0,0 +1,15 @@
+# Membuat huruf Z
+print("##########")
+print("       #  ")
+print("     #    ")
+print("   #      ")
+print(" #########")
+
+print()  # Baris kosong antara huruf Z dan I
+
+# Membuat huruf I
+print("##########")
+print("    #     ")
+print("    #     ")
+print("    #     ")
+print("##########")
