3579225a5319355052a6a1fc5b99a20fd3ac5d8d
ZavierGhani <zavierghani02@gmail.com>
2024-10-08 23:02:19 +0800

    Create PRAK005-2410817210014-ZAVIERPUTRANATAGHANI.c

---
+++ Modul-0/Soal 5/PRAK005-2410817210014-ZAVIERPUTRANATAGHANI.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // Membuat huruf Z
+    printf("##########\n");
+    printf("       #  \n");
+    printf("     #    \n");
+    printf("   #      \n");
+    printf(" #########\n");
+    
+    printf("\n"); // Baris kosong antara huruf Z dan I
+
+    // Membuat huruf I
+    printf("##########\n");
+    printf("    #     \n");
+    printf("    #     \n");
+    printf("    #     \n");
+    printf("##########\n");
+
+    return 0;
+}
