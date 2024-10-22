b127fb24067a321f1ed47d36c31e42029794c7fb
ZavierGhani <zavierghani02@gmail.com>
2024-10-22 23:36:09 +0800

    Create PRAK102-2410817210014-ZAVIER GHANI.py

---
+++ Modul-1/Soal 2/PRAK102-2410817210014-ZAVIER GHANI.py
@@ -0,0 +1,11 @@
+def cetak_pesan():
+    a = 4
+    b = 8
+    c = 3
+    hasil = a * b / c
+
+    print("Variabel a bernilai:", a)
+    print("Variabel b bernilai:", b)
+    print("Variabel c bernilai:", c)
+    print("Hasil dari a dikali b dibagi c adalah:", format(hasil, ".6f"))
+cetak_pesan()
