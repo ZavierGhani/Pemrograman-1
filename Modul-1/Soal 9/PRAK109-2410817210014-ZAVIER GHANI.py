6e7aa20756f9b7ccdb6dea36c4efed565f5a1c9b
ZavierGhani <zavierghani02@gmail.com>
2024-10-24 00:07:34 +0800

    Add files via upload

---
+++ Modul-1/Soal 9/PRAK109-2410817210014-ZAVIER GHANI.py
@@ -0,0 +1,10 @@
+def cetak_pesan():
+
+    jumlah_pasukan = 958730
+    jumlah_pahlawan = 5
+    pasukan_per_pahlawan = jumlah_pasukan // jumlah_pahlawan  
+    print(f"Jumlah pasukan yang dibawa Yu Zhong = {jumlah_pasukan}")
+    print(f"Jumlah pahlawan = {jumlah_pahlawan}")
+    print(f"Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah {pasukan_per_pahlawan} pasukan")
+
+cetak_pesan()
