013e79a4288d6a76404dc700a2cbb259191b27c0
ZavierGhani <zavierghani02@gmail.com>
2024-11-18 23:44:13 +0800

    Add files via upload

---
+++ Modul-2/Soal 1/PRAK201-2410817210014-ZAVIER GHANI.py
@@ -0,0 +1,19 @@
+def cetak_pesan():
+    nama = input("Masukkan Nama: ")
+    nim = input("Masukkan NIM: ")
+    kelas = input("Masukkan Kelas Paralel: ")
+    ttl = input("Masukkan Tempat/Tanggal Lahir: ")
+    alamat = input("Masukkan Alamat: ")
+    hobby = input("Masukkan Hobby: ")
+    no_hp = input("Masukkan No. HP: ")
+
+    print("\n=== Biodata ===")
+    print("Nama:", nama)
+    print("NIM:", nim)
+    print("Kelas Paralel:", kelas)
+    print("Tempat/Tanggal Lahir:", ttl)
+    print("Alamat:", alamat)
+    print("Hobby:", hobby)
+    print("No. HP:", no_hp)
+
+cetak_pesan() 
