a455735dc90e666453c289e7121e9485b2166d53
ZavierGhani <zavierghani02@gmail.com>
2024-11-19 11:29:09 +0800

    Add files via upload

---
+++ Modul 3/Soal 2/PRAK302-2410817210014-ZAVIER GHANI.py
@@ -0,0 +1,19 @@
+def konversi_nilai():
+
+    nilai = int(input("Masukkan nilai: "))
+
+    if nilai >= 80:
+        huruf = 'A'
+    elif nilai >= 70:
+        huruf = 'B'
+    elif nilai >= 60:
+        huruf = 'C'
+    elif nilai >= 50:
+        huruf = 'D'
+    else:
+        huruf = 'E'
+
+
+    print(huruf)
+
+konversi_nilai()
